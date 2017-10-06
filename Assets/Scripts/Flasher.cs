b7360c6339abd7024dd6c3f746f9a719cf622493
alelievr <alelievr@student.42.fr>
2017-10-06 15:14:48 +0200

    added Fasher and player controller

---
+++ Assets/Scripts/Flasher.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flasher : MonoBehaviour {
+
+	public bool		canRepeat;
+	public bool		triggerOnEnter = true;
+	public bool		triggerOnExit = false;
+	public bool		once = true;
+
+	[Space]
+	public float	delayTrigger = 0f;
+	public float	flashDuration = .2f;
+	public float	fadeOutDuration = .4f;
+
+	int				flashCount = 0;
+	SpriteRenderer	sp;
+
+	// Use this for initialization
+	void Start () {
+		sp = GetComponent< SpriteRenderer >();
+		sp.enabled = false;
+	}
+	
+	IEnumerator	FadeOut()
+	{
+		float startTime = Time.time;
+		float t = (Time.time - startTime) / fadeOutDuration;
+		while (t < 1f)
+		{
+			t = (Time.time - startTime) / fadeOutDuration;
+			sp.color = new Color(1f, 1f, 1f, Mathf.SmoothStep(1f, 0f, t));
+			yield return null;
+		}
+		sp.enabled = false;
+	}
+
+	IEnumerator FlashCoroutine()
+	{
+		if (once && flashCount > 0)
+			yield break;
+		
+		flashCount++;
+		if (delayTrigger > 0)
+			yield return new WaitForSeconds(delayTrigger);
+
+		sp.enabled = true;
+		yield return new WaitForSeconds(flashDuration);
+
+		if (fadeOutDuration > 0)
+			StartCoroutine(FadeOut());
+		else
+			sp.enabled = false;
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag != "Player")
+			return ;
+		if (triggerOnEnter)
+			StartCoroutine(FlashCoroutine());
+
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.tag != "Player")
+			return ;
+		
+		if (triggerOnExit)
+			StartCoroutine(FlashCoroutine());
+	}
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour {
+
+	public float	maxSpeed = 1f;
+	bool			facingRight = true;
+
+	[Space]
+	public Transform	groundCheck;
+	public LayerMask	groundMask;
+	bool				grounded = false;
+	public float		groundRadius = .2f;
+
+	[Space]
+	public float		jumpPower = 10f;
+	public float		jumpIdle = .3f;
+	bool				canJump = true;
+	
+	new Rigidbody2D	rigidbody2D;
+	SpriteRenderer	spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent< SpriteRenderer >();
+		rigidbody2D = GetComponent< Rigidbody2D >();
+	}
+
+	void FixedUpdate()
+	{
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundMask);
+
+		float move = Input.GetAxis("Horizontal");
+
+		rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+
+		if (move > 0 && !facingRight)
+			Flip();
+		else if (move < 0 && facingRight)
+			Flip();
+	}
+
+	void Flip()
+	{
+		facingRight = !facingRight;
+		spriteRenderer.flipY = facingRight;
+	}
+
+	IEnumerator JumpDelay()
+	{
+		canJump = false;
+		yield return new WaitForSeconds(jumpIdle);
+		canJump = true;
+	}
+	
+	void Update () {
+		if (grounded && Input.GetKeyDown(KeyCode.Space) && canJump)
+		{
+			rigidbody2D.AddForce(new Vector2(0, jumpPower), ForceMode2D.Impulse);
+			StartCoroutine(JumpDelay());
+		}
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireSphere(groundCheck.position, groundRadius);
+	}
+}
