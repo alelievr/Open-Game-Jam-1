64778fbaa2d907d0c965912f35ae4a6dbe008cfc
Gabriel FOURNIER <gfournie@e1r11p6.42.fr>
2017-10-06 17:00:10 +0200

    fds

---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour {
+
+	public GameObject who;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		this.transform.position = who.transform.position + new Vector3(0f, 0f, -5f);		
+	}
+}
---
+++ Assets/Scripts/Stopmoving.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stopmoving : MonoBehaviour {
+
+	public bool cannotmove = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public bool Stop()
+	{
+		return cannotmove;
+	}
+}
