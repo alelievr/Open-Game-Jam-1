dfa783e1eae6c0b93846d8a2f6e5d20cc6076aa2
alelievr <alelievr@student.42.fr>
2017-10-08 02:25:11 +0200

    added MarkAtor

---
+++ Assets/Scripts/Player/Markator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Markator : MonoBehaviour {
+
+	public ParticleSystem			splatParticles;
+
+	new ParticleSystem				particleSystem;
+	List< ParticleCollisionEvent >	collisionEvents = new List< ParticleCollisionEvent >();
+
+	void Start () {
+
+		particleSystem = GetComponent< ParticleSystem >();
+	}
+	
+	void OnParticleCollision(GameObject other)
+	{
+		if (other.tag == "Enemy")
+			Destroy(other);
+		else
+		{
+			ParticlePhysicsExtensions.GetCollisionEvents(particleSystem, other, collisionEvents);
+
+			for (int i = 0; i < collisionEvents.Count; i++)
+				EmitAtLocation(collisionEvents[i]);
+		}
+	}
+
+	void EmitAtLocation(ParticleCollisionEvent pce)
+	{
+		splatParticles.transform.position = pce.intersection;
+		splatParticles.transform.rotation = Quaternion.identity;
+		splatParticles.Emit(1);
+	}
+}
