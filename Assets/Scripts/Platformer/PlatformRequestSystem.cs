ed0db65191d452492136b241dba1cb7aafc3300d
jromagna42 <jromagna@student.42.fr>
2017-10-08 11:42:46 +0200

    platform animation

--- Assets/Scripts/Platformer/PlatformRequestSystem.cs
+++ Assets/Scripts/Platformer/PlatformRequestSystem.cs
@@ -20,6 +20,8 @@ public class PlatformRequestSystem : MonoBehaviour {
 		public int					availableNumber = 1;
 		public float				timeBeforeDispawn = 4;
 		public GameObject			platformPrefab;
+		public GameObject			platformMarkPrefab;
+		public GameObject			PsPrefab;
 	}
 
 	public PlatformKey	defaultPlatform;
@@ -47,21 +49,33 @@ public class PlatformRequestSystem : MonoBehaviour {
 		camera = Camera.main;
 	}
 
+	IEnumerator DelayPlatformSpawn(Transform psChild)
+	{
+		yield return new WaitForSeconds(0.2f);
+		psChild.gameObject.SetActive(true);
+		Platform pf = psChild.gameObject.GetComponent< Platform >();
+		pf.Init(currentPlatformKey.timeBeforeDispawn);
+		ParticleSystem	ps = psChild.gameObject.GetComponent< ParticleSystem >();
+		ps.Play();
+		currentPlatformKey.availableNumber--;
+		OnPlatformPlaced(currentPlatformKey.type, currentPlatformKey.availableNumber);
+		StartCoroutine(RefillPlatform(currentPlatformKey));
+		showPreview = false;
+	}
+
 	void Update()
 	{
 		foreach (var kp in platforms)
 		{
 			if (Input.GetKeyDown(kp.key) && kp.availableNumber > 0)
 			{
-				//destroy last object
+				// destroy last object
 				if (showPreview && previewGO != null)
 					Destroy(previewGO);
-				
-				previewGO = Instantiate(kp.platformPrefab);
-				Platform p = previewGO.GetComponent< Platform >();
-				p.timeBeforeDispawn = kp.timeBeforeDispawn;
-				p.enabled = false;
-				previewGO.GetComponent< Collider2D >().enabled = false;
+			
+				previewGO = Instantiate(kp.platformMarkPrefab);
+				// Platform p = previewGO.GetComponent< Platform >();
+				// previewGO.GetComponent< Collider2D >().enabled = false;
 				currentPlatformKey = kp;
 				showPreview = true;
 			}
@@ -76,14 +90,22 @@ public class PlatformRequestSystem : MonoBehaviour {
 			//place platform by pressing enter, return or mouse left click
 			if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetMouseButtonDown(0))
 			{
-				previewGO.GetComponent< Collider2D >().enabled = true;
-				ParticleSystem	ps = previewGO.GetComponent< ParticleSystem >();
-				ps.Simulate(2f);
-				ps.Play();
-				currentPlatformKey.availableNumber--;
-				OnPlatformPlaced(currentPlatformKey.type, currentPlatformKey.availableNumber);
-				StartCoroutine(RefillPlatform(currentPlatformKey));
-				showPreview = false;
+
+				GameObject psRef = Instantiate(currentPlatformKey.PsPrefab);
+				Transform psChild = psRef.transform.GetChild(0);
+				psRef.transform.position = (p + (Vector3.up * 10.2f));
+				Destroy(previewGO);
+				StartCoroutine(DelayPlatformSpawn(psChild));
+				// previewGO.GetComponent< Collider2D >().enabled = true;
+				// Platform pf = previewGO.GetComponent< Platform >();
+				// pf.Init(currentPlatformKey.timeBeforeDispawn);
+				// ParticleSystem	ps = previewGO.GetComponent< ParticleSystem >();
+				// ps.Simulate(2f);
+				// ps.Play();
+				// currentPlatformKey.availableNumber--;
+				// OnPlatformPlaced(currentPlatformKey.type, currentPlatformKey.availableNumber);
+				// StartCoroutine(RefillPlatform(currentPlatformKey));
+				// showPreview = false;
 			}
 		}
 
