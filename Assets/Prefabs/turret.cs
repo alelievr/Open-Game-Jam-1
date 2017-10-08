761e95c8ca94a4f85a46bf93c3864d7a1e56f38a
Gabriel FOURNIER <gabfou95@gmail.com>
2017-10-08 22:03:08 +0200

    fdsf

---
+++ Assets/Prefabs/turret.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turret : MonoBehaviour {
+	public GameObject bullet;
+	float timesinceprevious;
+	public float spawntime;
+	public float range;
+	LayerMask l;
+	// Use this for initialization
+	void Start () {
+		l = ~((1 << 8) | (1 << 9) | (1 << 10));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timesinceprevious += Time.deltaTime;
+
+		if (timesinceprevious > spawntime)
+		{
+			RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.Normalize(Camera.main.transform.position - transform.position), range);
+			if (hit.collider != null && hit.collider.tag == "Player") {
+				GameObject.Instantiate (bullet, transform.position, Quaternion.identity);
+			}
+			timesinceprevious = 0;
+		}
+	}
+}
